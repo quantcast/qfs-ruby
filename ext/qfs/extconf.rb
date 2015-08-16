da38726352a15a7d4b6763253baf30597854ef83
Eric Culp <eculp@quantcast.com>
2015-08-16 15:02:47 -0700

    Add more options to gem configuration

--- ext/qfs/extconf.rb
+++ ext/qfs/extconf.rb
@@ -1,6 +1,18 @@
 require 'mkmf'
 
+INCLUDE_DIRS = [
+	Config::CONFIG['includedir'],
+	'/usr/include',
+]
+
+LIB_DIRS = [
+	Config::CONFIG['libdir'],
+	'/usr/lib',
+]
+
+dir_config 'qfs', INCLUDE_DIRS, LIB_DIRS
+
 abort '"kfs/c/qfs.h" is required' unless find_header 'kfs/c/qfs.h'
-abort 'libkfs_access is required' unless find_library 'qfsc', 'qfs_open'
+abort 'libqfsc is required' unless find_library 'qfsc', 'qfs_open'
 
 create_makefile 'qfs_ext'
