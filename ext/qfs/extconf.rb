d37887ab7d30d7d96202029373184d029751689c
Eric Culp <eculp@quantcast.com>
2015-08-16 17:04:52 -0700

    Change ordering of LOCAL_LIBS

--- ext/qfs/extconf.rb
+++ ext/qfs/extconf.rb
@@ -13,11 +13,11 @@ LIB_DIRS = [
 
 dir_config 'qfs', INCLUDE_DIRS, LIB_DIRS
 
-abort '"kfs/c/qfs.h" is required' unless find_header 'kfs/c/qfs.h'
-abort 'libqfsc is required' unless find_library 'qfsc', 'qfs_open'
-
 with_config('qfs-local-libs', '').split(':').each do |lib|
 	$LOCAL_LIBS << lib
 end
+
+abort '"kfs/c/qfs.h" is required' unless find_header 'kfs/c/qfs.h'
+abort 'libqfsc is required' unless find_library 'qfsc', 'qfs_open'
 
 create_makefile 'qfs_ext'
