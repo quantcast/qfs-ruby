7c2e7c3d02febdfc02f9116097724832f01d6663
Eric Culp <eculp@quantcast.com>
2015-08-20 12:16:40 -0700

    Add support for linker version script

--- ext/qfs/extconf.rb
+++ ext/qfs/extconf.rb
@@ -17,6 +17,10 @@ with_config('qfs-local-libs', '').split(':').each do |lib|
 	$LOCAL_LIBS << " #{lib} "
 end
 
+if with_config('version-script')
+	$LDFLAGS << " -Wl,--version-script=qfs_ext.version "
+end
+
 abort '"kfs/c/qfs.h" is required' unless find_header 'kfs/c/qfs.h'
 abort 'libqfsc is required' unless find_library 'qfsc', 'qfs_open'
 
