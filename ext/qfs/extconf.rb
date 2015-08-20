497a6568aff05d0e9c959eb9b4dc4b05b9af892a
Eric Culp <eculp@quantcast.com>
2015-08-20 12:31:28 -0700

    Use absolute path for version script

--- ext/qfs/extconf.rb
+++ ext/qfs/extconf.rb
@@ -17,8 +17,8 @@ with_config('qfs-local-libs', '').split(':').each do |lib|
 	$LOCAL_LIBS << " #{lib} "
 end
 
-if with_config('version-script')
-	$LDFLAGS << " -Wl,--version-script=qfs_ext.version "
+if with_config('version-script', '0') == '1'
+	$LDFLAGS << " -Wl,--version-script=#{File.expand_path('../qfs_ext.version', __FILE__)} "
 end
 
 abort '"kfs/c/qfs.h" is required' unless find_header 'kfs/c/qfs.h'
